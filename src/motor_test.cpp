5becd903d9f2d1b1b900ce0dbdc16ae7ab297e5b
Xyntexx <Xyntexx@users.noreply.github.com>
2025-04-22 19:41:39 +0300

    Add PWM speed control to motor testing Fix upload protocol typo in platformio.ini

--- src/motor_test.cpp
+++ src/motor_test.cpp
@@ -1,9 +1,15 @@
-
-
 #include "motor_test.h"
 #include "Arduino.h"
 #include "defines.h"
 
+// PWM speed control constants
+const int TEST_SPEEDS[] = {
+  static_cast<int>(MAX_PWM * 0.25),  // 25% of max
+  static_cast<int>(MAX_PWM * 0.50),  // 50% of max
+  static_cast<int>(MAX_PWM * 0.75),  // 75% of max
+  static_cast<int>(MAX_PWM)          // 100% of max
+};
+const int NUM_SPEEDS = sizeof(TEST_SPEEDS) / sizeof(TEST_SPEEDS[0]);
 
 void print_motor_current() {
   int current = analogRead(MOTOR_CURRENT_PIN);
@@ -16,7 +22,7 @@ void print_motor_current() {
   pinMode(MOTOR_ENABLE_PIN, OUTPUT);
   pinMode(MOTOR_PWM_PIN, OUTPUT);
   pinMode(MOTOR_DIR_PIN, OUTPUT);
-  pinMode(MOTOR_CURRENT_PIN, INPUT);
+  pinMode(MOTOR_CURRENT_PIN, ANALOG);
   digitalWrite(MOTOR_ENABLE_PIN, LOW);
   digitalWrite(MOTOR_PWM_PIN, LOW);
   digitalWrite(MOTOR_DIR_PIN, LOW);
@@ -28,39 +34,53 @@ void print_motor_current() {
       print_motor_current();
       delay(200);
     }
-    LOGSerial.println("Motor braking");
-    digitalWrite(MOTOR_ENABLE_PIN, HIGH);
-    for (int i = 0; i < 5; i++) {
-      print_motor_current();
-      delay(100);
+
+    // Test different speeds in forward direction
+    for (int speed_idx = 0; speed_idx < NUM_SPEEDS; speed_idx++) {
+      int speed = TEST_SPEEDS[speed_idx];
+      LOGSerial.print("Motor forward at speed: ");
+      LOGSerial.println(speed);
+      
+      digitalWrite(MOTOR_ENABLE_PIN, HIGH);
+      digitalWrite(MOTOR_DIR_PIN, HIGH);
+      analogWrite(MOTOR_PWM_PIN, speed);
+      
+      for (int i = 0; i < 5; i++) {
+        print_motor_current();
+        delay(100);
+      }
+      
+      // Brake between speed changes
+      LOGSerial.println("Motor braking");
+      digitalWrite(MOTOR_PWM_PIN, LOW);
+      for (int i = 0; i < 5; i++) {
+        print_motor_current();
+        delay(100);
+      }
     }
-    LOGSerial.println("Motor forward");
-    digitalWrite(MOTOR_PWM_PIN, HIGH);
-    digitalWrite(MOTOR_DIR_PIN, HIGH);
-    for (int i = 0; i < 5; i++) {
-      print_motor_current();
-      delay(100);
-    }
-    LOGSerial.println("Motor braking");
-    digitalWrite(MOTOR_PWM_PIN, LOW);
-    digitalWrite(MOTOR_DIR_PIN, LOW);
-    for (int i = 0; i < 5; i++) {
-      print_motor_current();
-      delay(100);
-    }
-    LOGSerial.println("Motor reverse");
-    digitalWrite(MOTOR_PWM_PIN, HIGH);
-    digitalWrite(MOTOR_DIR_PIN, LOW);
-    for (int i = 0; i < 5; i++) {
-      print_motor_current();
-      delay(100);
-    }
-    LOGSerial.println("Motor braking");
-    digitalWrite(MOTOR_PWM_PIN, LOW);
-    digitalWrite(MOTOR_DIR_PIN, LOW);
-    for (int i = 0; i < 5; i++) {
-      print_motor_current();
-      delay(100);
+
+    // Test different speeds in reverse direction
+    for (int speed_idx = 0; speed_idx < NUM_SPEEDS; speed_idx++) {
+      int speed = TEST_SPEEDS[speed_idx];
+      LOGSerial.print("Motor reverse at speed: ");
+      LOGSerial.println(speed);
+      
+      digitalWrite(MOTOR_ENABLE_PIN, HIGH);
+      digitalWrite(MOTOR_DIR_PIN, LOW);
+      analogWrite(MOTOR_PWM_PIN, speed);
+      
+      for (int i = 0; i < 5; i++) {
+        print_motor_current();
+        delay(100);
+      }
+      
+      // Brake between speed changes
+      LOGSerial.println("Motor braking");
+      digitalWrite(MOTOR_PWM_PIN, LOW);
+      for (int i = 0; i < 5; i++) {
+        print_motor_current();
+        delay(100);
+      }
     }
   }
 }
--- src/motor_test.h
+++ src/motor_test.h
@@ -3,6 +3,8 @@
 #ifndef MOTOR_TEST_H_
 #define MOTOR_TEST_H_
 
+#define MAX_PWM 255 * 0.50 // lower power for testing
+
 void init_motor_test();
 
 #endif // MOTOR_TEST_H_
